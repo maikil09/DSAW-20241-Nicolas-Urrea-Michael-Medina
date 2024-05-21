0e3ee1d912decad698a5b84742c44f820f0efc26
mike_medina <maicolmedinafernandez@gmail.com>
2024-05-21 10:17:25 -0500

    correccion

--- front-APP/src/main.tsx
+++ front-APP/src/main.tsx
@@ -1,43 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Login from './routes/Login.tsx'
-import Signup from './routes/Signup.tsx'
-import Home from './routes/Home.tsx'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import ProtectedRoute from './routes/ProtectedRoute.tsx'
-import { AuthProvider } from './Auth/AuthProvider.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './routes/Login';
+import Signup from './routes/Signup';
+import Home from './routes/Home';
+import ProtectedRoute from './routes/ProtectedRoute';
+import { AuthProvider } from './Auth/AuthProvider';
+import './index.css';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Login />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/signup",
-    element: <Signup/>,
-  },
-  {
-    path: "/",
-    element: <ProtectedRoute/>,
-    children: [
-      {
-        path: "/home",
-        element: <Home/>,
-      },
-    ],
-  },
+const App = () => (
+  <AuthProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/home" element={<Home />} />
+        </Route>
+      </Routes>
+    </Router>
+  </AuthProvider>
+);
 
-]);
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 );
